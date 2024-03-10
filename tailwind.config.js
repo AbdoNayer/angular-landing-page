c64ec9ee9793dc128d78efa2971fb3b2ac7f09f1
aNayer <anayer@tryblend.net>
2024-03-10 23:02:42 +0200

    fix-screen-mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,31 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2lg': {'max': '2000px'},
+        'lg': {'max': '1280px'},
+        'md': {'max': '767px'},
+        'sm': {'max': '500px'}
+      }
+    },
     container: {
       center: true,
+      screens: {
+        'sm': {'min': '640px'},
+        'md': {'min': '768px'},
+        'lg': {'min': '1024px'},
+        'xl': {'min': '1280px'},
+        '2xl': {'min': '1536px'},
+      },
       padding: {
         DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        sm: '1rem',
+        lg: '2rem',
+        xl: '4rem',
+        '2xl': '5rem',
       },
     },
-    screens: {
-      '2lg': {'max': '2000px'},
-      'lg': {'max': '1100px'},
-      'md': {'max': '767px'},
-      'sm': {'max': '500px'}
-    }
   },
   plugins: [],
 }
