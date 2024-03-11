d1e997e134858d2ad4e79a207b71f3068fbd93fd
aNayer <anayer@tryblend.net>
2024-03-12 01:25:59 +0200

    add-ViewScrollerSections

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from "@angular/common";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  
+  constructor(private scroller: ViewportScroller) {}
+
+  ngOnInit() {}
+
+  goDown(val: any) {
+    document.getElementById(val)!.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+  }
 
 }
